d46c8fd9e94eb89c290c9f102e3068d17d3a2d7a
SubhasisMukherjee <subhtech09@gmail.com>
2023-11-12 22:14:03 +0530

    Incorporating - Exception handling - Adding extra modules - Regenerating the requirements.txt

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from tkinter import messagebox as mb
 from tkinter import filedialog as fd
 from pytube import YouTube
 from PIL import Image, ImageTk
+import traceback as tb
+from colorama import Fore
+from utilities.contants import PRINT_DETAILED_EXCEPTION
 
 
 def browse_folder():
@@ -85,101 +88,109 @@ def exit():
 
 
 if __name__ == "__main__":
-    # Create an object of the Tk() class
-    gui_root = Tk()
 
-    # Set the title of the window
-    gui_root.title("YouTube Downloader")
+    try:
+        # Create an object of the Tk() class
+        gui_root = Tk()
 
-    # Set the size and position of the window
-    gui_root.geometry("580x220+300+150")
+        # Set the title of the window
+        gui_root.title("YouTube Downloader")
 
-    # Disable the resizable option for better UI
-    gui_root.resizable(0, 0)
+        # Set the size and position of the window
+        gui_root.geometry("580x220+300+150")
 
-    # Configure the background color of the window
-    gui_root.config(bg="#FEE4E3")
+        # Disable the resizable option for better UI
+        gui_root.resizable(0, 0)
 
-    # Configure the icon of the window
-    gui_root.iconbitmap("images/download.ico")
+        # Configure the background color of the window
+        gui_root.config(bg="#FEE4E3")
 
-    # Add 3 frames to the window using the Frame() widget
-    header_frame = Frame(gui_root, bg="#FEE4E3")
-    entry_frame = Frame(gui_root, bg="#FEE4E3")
-    button_frame = Frame(gui_root, bg="#FEE4E3")
-    header_frame.pack()
-    entry_frame.pack()
-    button_frame.pack()
+        # Configure the icon of the window
+        gui_root.iconbitmap("images/download.ico")
 
-    # ------------------------- the Header Frame -------------------------
+        # Add 3 frames to the window using the Frame() widget
+        header_frame = Frame(gui_root, bg="#FEE4E3")
+        entry_frame = Frame(gui_root, bg="#FEE4E3")
+        button_frame = Frame(gui_root, bg="#FEE4E3")
+        header_frame.pack()
+        entry_frame.pack()
+        button_frame.pack()
 
-    # Import the youtube logo bitmap image
-    the_image = ImageTk.PhotoImage(Image.open("images/youtube_logo.png").resize((50, 35)))
+        # ------------------------- the Header Frame -------------------------
 
-    # Add the labels to the header frame
-    image_label = Label(header_frame, image=the_image, bg="#FEE4E3", fg="#FE0700", anchor=SE)
-    header_label = Label(header_frame, text="YouTube Video Downloader", font=("verdana", "14", "bold"), bg="#FEE4E3", anchor=SE)
+        # Import the youtube logo bitmap image
+        the_image = ImageTk.PhotoImage(Image.open("images/youtube_logo.png").resize((50, 35)))
 
-    # Set the position of the labels in the grid format
-    image_label.grid(row=0, column=0, padx=10, pady=10)
-    header_label.grid(row=0, column=1, padx=10, pady=10)
+        # Add the labels to the header frame
+        image_label = Label(header_frame, image=the_image, bg="#FEE4E3", fg="#FE0700", anchor=SE)
+        header_label = Label(header_frame, text="YouTube Video Downloader", font=("verdana", "14", "bold"), bg="#FEE4E3", anchor=SE)
 
-    # ------------------------- The Entry Frame -------------------------
+        # Set the position of the labels in the grid format
+        image_label.grid(row=0, column=0, padx=10, pady=10)
+        header_label.grid(row=0, column=1, padx=10, pady=10)
 
-    # Add the labels to the entry frame
-    url_label = Label(entry_frame, text="Video URL:", font=("verdana", "10"), bg="#FEE4E3", fg="#000000", anchor=SE)
-    des_label = Label(entry_frame, text="Destination:", font=("verdana", "10"), bg="#FEE4E3", fg="#000000", anchor=SE)
+        # ------------------------- The Entry Frame -------------------------
 
-    # Set the position of the labels in the grid format
-    url_label.grid(row=0, column=0, padx=5, pady=5, sticky=E)
-    des_label.grid(row=1, column=0, padx=5, pady=5, sticky=E)
+        # Add the labels to the entry frame
+        url_label = Label(entry_frame, text="Video URL:", font=("verdana", "10"), bg="#FEE4E3", fg="#000000", anchor=SE)
+        des_label = Label(entry_frame, text="Destination:", font=("verdana", "10"), bg="#FEE4E3", fg="#000000", anchor=SE)
 
-    # Create objects to hold video URL and download path
-    video_url = StringVar()
-    download_dir = StringVar()
+        # Set the position of the labels in the grid format
+        url_label.grid(row=0, column=0, padx=5, pady=5, sticky=E)
+        des_label.grid(row=1, column=0, padx=5, pady=5, sticky=E)
 
-    # Add entry fields for video URL and downlaod path in the entry frame
-    url_field = Entry(entry_frame, textvariable=video_url, width=35, font=("verdana", "10"),
-                      bg="#FFFFFF", fg="#000000", relief=GROOVE)
-    des_field = Entry(entry_frame, textvariable=download_dir, width=26, font=("verdana", "10"),
-                      bg="#FFFFFF", fg="#000000", relief=GROOVE)
+        # Create objects to hold video URL and download path
+        video_url = StringVar()
+        download_dir = StringVar()
 
-    # Set the position of the entry fields in the grid format
-    url_field.grid(row=0, column=1, padx=5, pady=5, columnspan=2)
-    des_field.grid(row=1, column=1, padx=5, pady=5)
+        # Add entry fields for video URL and downlaod path in the entry frame
+        url_field = Entry(entry_frame, textvariable=video_url, width=35, font=("verdana", "10"),
+                          bg="#FFFFFF", fg="#000000", relief=GROOVE)
+        des_field = Entry(entry_frame, textvariable=download_dir, width=26, font=("verdana", "10"),
+                          bg="#FFFFFF", fg="#000000", relief=GROOVE)
 
-    # Add a button to the entry frame to browse folder path
-    # Set it to trigger 'browse_folder' function on click
-    browse_button = Button(entry_frame, text="Browse", width=7, font=("verdana", "10"), bg="#FF9200",
-                           fg="#FFFFFF", activebackground="#FFE0B7", activeforeground="#000000",
-                           relief=GROOVE, command=browse_folder)
+        # Set the position of the entry fields in the grid format
+        url_field.grid(row=0, column=1, padx=5, pady=5, columnspan=2)
+        des_field.grid(row=1, column=1, padx=5, pady=5)
 
-    # Set the position of the button in the grid format
-    browse_button.grid(row=1, column=2, padx=5, pady=5)
+        # Add a button to the entry frame to browse folder path
+        # Set it to trigger 'browse_folder' function on click
+        browse_button = Button(entry_frame, text="Browse", width=7, font=("verdana", "10"), bg="#FF9200",
+                               fg="#FFFFFF", activebackground="#FFE0B7", activeforeground="#000000",
+                               relief=GROOVE, command=browse_folder)
 
-    # ------------------------- The Button Frame -------------------------
+        # Set the position of the button in the grid format
+        browse_button.grid(row=1, column=2, padx=5, pady=5)
 
-    # Add a button to the button frame to download video
-    # Set it to trigger 'download_video' function on click
-    download_button = Button(button_frame, text="Download", width=12, font=("verdana", "10"),
-                             bg="#15EF5F", fg="#FFFFFF", activebackground="#97F9B8", activeforeground="#000000",
-                             relief=GROOVE, command=download_video)
+        # ------------------------- The Button Frame -------------------------
 
-    # Add a button to the button frame to reset the entry fields
-    # Set it to trigger 'reset' function on click
-    reset_button = Button(button_frame, text="Clear", width=12, font=("verdana", "10"), bg="#23B1E6",
-                          fg="#FFFFFF", activebackground="#C3E6EF", activeforeground="#000000",
-                          relief=GROOVE, command=reset)
-    # Add a button to the button frame to close the application
-    # Set it to trigger 'exit' function on click
-    close_button = Button(button_frame, text="Exit", width=12, font=("verdana", "10"), bg="#F64247",
-                          fg="#FFFFFF", activebackground="#F7A2A5", activeforeground="#000000",
-                          relief=GROOVE, command=exit)
+        # Add a button to the button frame to download video
+        # Set it to trigger 'download_video' function on click
+        download_button = Button(button_frame, text="Download", width=12, font=("verdana", "10"),
+                                 bg="#15EF5F", fg="#FFFFFF", activebackground="#97F9B8", activeforeground="#000000",
+                                 relief=GROOVE, command=download_video)
 
-    # Set the position of the buttons in the grid format
-    download_button.grid(row=0, column=0, padx=5, pady=10)
-    reset_button.grid(row=0, column=1, padx=5, pady=10)
-    close_button.grid(row=0, column=2, padx=5, pady=10)
+        # Add a button to the button frame to reset the entry fields
+        # Set it to trigger 'reset' function on click
+        reset_button = Button(button_frame, text="Clear", width=12, font=("verdana", "10"), bg="#23B1E6",
+                              fg="#FFFFFF", activebackground="#C3E6EF", activeforeground="#000000",
+                              relief=GROOVE, command=reset)
+        # Add a button to the button frame to close the application
+        # Set it to trigger 'exit' function on click
+        close_button = Button(button_frame, text="Exit", width=12, font=("verdana", "10"), bg="#F64247",
+                              fg="#FFFFFF", activebackground="#F7A2A5", activeforeground="#000000",
+                              relief=GROOVE, command=exit)
 
-    # Trigger mainloop() method
-    gui_root.mainloop()
+        # Set the position of the buttons in the grid format
+        download_button.grid(row=0, column=0, padx=5, pady=10)
+        reset_button.grid(row=0, column=1, padx=5, pady=10)
+        close_button.grid(row=0, column=2, padx=5, pady=10)
+
+        # Trigger mainloop() method
+        gui_root.mainloop()
+
+    except Exception as excp:
+        if PRINT_DETAILED_EXCEPTION:
+            tb.print_exc()
+        else:
+            print(Fore.RED, f'\nException occured: {excp.__str__()}')
